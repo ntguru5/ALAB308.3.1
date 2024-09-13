da59f922424e6ee4fab9e1c411bf51582863bdf4
Ken Yeh <yehken@gmail.com>
2024-09-12 20:14:21 -0700

    Fizz Buzz complete

--- exercise.js
+++ exercise.js
@@ -1,5 +1,19 @@
+// PART 1 Fizzbuzz
+
 // Loop through all numbers from 1 to 100.
 // If a number is divisible by 3, log “Fizz.”
 // If a number is divisible by 5, log “Buzz.”
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
+
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("Fizz Buzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+}
