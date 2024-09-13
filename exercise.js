2a3470e0dc7f6dd26797bc21d4020f4f35665896
Ken Yeh <yehken@gmail.com>
2024-09-12 20:48:12 -0700

    part 2 Prime Time complete

--- exercise.js
+++ exercise.js
@@ -6,22 +6,22 @@
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
 
-for (let num = 1; num <= 100; num++) {
-    if (num % 3 === 0 && num % 5 === 0) {
-        console.log("Fizz Buzz");
-    } else if (num % 3 === 0) {
-        console.log("Fizz");
-    } else if (num % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(num);
-    }
-}
+// for (let num = 1; num <= 100; num++) {
+//     if (num % 3 === 0 && num % 5 === 0) {
+//         console.log("Fizz Buzz");
+//     } else if (num % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (num % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(num);
+//     }
+// }
 
-// alternative Fizz Buzz solution
-for (let num = 1; num <= 100; num++) {
-    console.log((num % 3 === 0 ? 'Fizz ' : '') + (num % 5 === 0 ? 'Buzz' : '') || num);
-}
+// // // alternative Fizz Buzz solution
+// for (let num = 1; num <= 100; num++) {
+//     console.log((num % 3 === 0 ? 'Fizz ' : '') + (num % 5 === 0 ? 'Buzz' : '') || num);
+// }
 
 // PART 2 Prime Time
 // Declare an arbitrary number, n.
@@ -29,3 +29,21 @@ for (let num = 1; num <= 100; num++) {
 // As soon as you find the prime number, log that number and exit the loop.
 
 let n = 9;
+
+// Loop indefinitely
+for (let num = n + 1;;num++) {
+    let isPrime = true;
+
+    if (num > 1) {
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            console.log(num + " is the next prime number");
+            break;
+        }
+    }
+}
