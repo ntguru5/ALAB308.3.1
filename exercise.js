0d60dc36ca4a4b06f113cf9007ca5606917ee001
Ken Yeh <yehken@gmail.com>
2024-09-12 22:40:30 -0700

    start part 3 Feeling Loopy

--- exercise.js
+++ exercise.js
@@ -1,4 +1,4 @@
-// PART 1 Fizzbuzz
+// ================== PART 1 Fizzbuzz
 
 // Loop through all numbers from 1 to 100.
 // If a number is divisible by 3, log “Fizz.”
@@ -6,24 +6,24 @@
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
 
-// for (let num = 1; num <= 100; num++) {
-//     if (num % 3 === 0 && num % 5 === 0) {
-//         console.log("Fizz Buzz");
-//     } else if (num % 3 === 0) {
-//         console.log("Fizz");
-//     } else if (num % 5 === 0) {
-//         console.log("Buzz");
-//     } else {
-//         console.log(num);
-//     }
-// }
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("Fizz Buzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+}
 
 // // // alternative Fizz Buzz solution
-// for (let num = 1; num <= 100; num++) {
-//     console.log((num % 3 === 0 ? 'Fizz ' : '') + (num % 5 === 0 ? 'Buzz' : '') || num);
-// }
+for (let num = 1; num <= 100; num++) {
+    console.log((num % 3 === 0 ? 'Fizz ' : '') + (num % 5 === 0 ? 'Buzz' : '') || num);
+}
 
-// PART 2 Prime Time
+// ================== PART 2 Prime Time
 // Declare an arbitrary number, n.
 // Create a loop that searches for the next prime number, starting at n and incrementing from there.
 // As soon as you find the prime number, log that number and exit the loop.
@@ -47,3 +47,19 @@ for (let num = n + 1;;num++) {
         }
     }
 }
+
+// ================== PART 3 Feeling Loopy
+// Loop through the characters of a given CSV string.
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
+// You can make the following assumptions:
+// There will only be 4 cells per row.
+// There will be no escaped characters other than “\n”.
+
+// Example CSV string
+let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
